d2a531b1df1588abaf376a891e8fdeeacd4b2957
emmekappaa <michele.cipriani.mm61@gmail.com>
2023-12-12 15:55:23 +0100

    Cleaned Code

--- src/main.cpp
+++ src/main.cpp
@@ -42,38 +42,5 @@ int main(int argc, char const *argv[])
             // cin >> stop;
         }
     }
-
-    /*
-    Node q1("q1", "first node");
-    /Node q2("q2", "second node");
-    Node q3("q3", "third node");
-
-    q1.addTransition("(x>=5)", q2.getName());  // q1 -> q2
-    q2.addTransition("(x>=10)", q3.getName()); // q2 -> q3
-    q2.addTransition("(x<5)", q1.getName());
-
-    q3.addTransition("(x<5)", q1.getName());
-    vector<Node> nodes = {q1, q2, q3};
-    Node initialNode = q1;
-    vector<Node> finalNodes = {q3};
-    unordered_map<string, double *> variables;
-    double x = 1.56;
-    double y = 2.0;
-    variables["x"] = &x;
-    variables["y"] = &y;
-    Status status = RUNNING;
-    Automata automata(nodes, initialNode, finalNodes, variables, status);
-    automata.setCurrentNode(initialNode);
-    cout << automata;
-    */
-
-    // for rand
-    // Declaring the upper and lower
-    // bounds
-    // double lower_bound = 0;
-    // double upper_bound = 15;
-    // uniform_real_distribution<double> unif(lower_bound, upper_bound);
-    // default_random_engine re;
-
     return 0;
 }
