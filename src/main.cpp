6fd18c3110c2d5d75a462e8ddccb1bcdce51ca4f
Alessandro Aldegheri <alessandroaldegherivr@gmail.com>
2023-12-21 16:21:05 +0100

    pseudocode scheduling

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,34 @@ int main(int argc, char const *argv[])
     for (double time = 1; time < finaltime; time = time + delta)
     {
 
+        /*
+            secondo me ci servirà una mappa delle "dipendenze", ovvero x è calcolato nell'automa a, d in b....
+            x -> a
+            d -> b
+            c -> d
+
+            ci servirà una mappa che si aggiorna ad ogni time, con automa e le istruzioni del suo nodo corrente
+            hashmap <automa-instructions>
+
+
+            l'idea è di avere un grafo con gli automi come nodi e poi gli archi rappresentano le dipendenze, top-sort, abbiamo l'ordine
+            (chatgpt dice che ha senso)
+            guardatevi al volo algoritmisacrograal che in 3 secondi lo riprendete
+            vi lascio uno pseudocodice
+
+            crea grafo dell'istante
+            per ognuno di questi:
+                crea nodo nel grafo
+                cicla ogni istruzione:
+                    aggiungi arco (a->b, a va eseguito prima di b)
+            calcola topological sort
+            seguendo l'ordine esegui gli automi
+
+
+
+
+        */
+
         for (int j = 0; j < v.size(); j++)
         {
             v[j].checkForChanges();
