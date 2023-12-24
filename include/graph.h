12a0d9edfb70515ad1af8e390f903292e7b987f9
Alessandro Aldegheri <alessandroaldegherivr@gmail.com>
2023-12-24 10:44:08 +0100

    graph + topological sort
    Co-authored-by: Davide Venturi <notBiji@users.noreply.github.com>

---
+++ include/graph.h
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+// Forward declaration of GraphEdge class
+class GraphEdge;
+
+/// @brief class that represents a node in the graph
+class GraphNode
+{
+public:
+    string nodeName;
+    vector<GraphEdge *> outgoingEdges;
+    GraphNode(string name);
+};
+
+/// @brief class that represents a edge in the graph
+class GraphEdge
+{
+public:
+    GraphNode *source;
+    GraphNode *target;
+    GraphEdge(GraphNode *source, GraphNode *target);
+};
+
+/// @brief class that represents the automatas as a dag
+class Graph
+{
+private:
+    bool visitedNode(GraphNode *node, unordered_map<string, int> &visited, stack<GraphNode *> &stack);
+
+public:
+    vector<GraphNode *> nodes;
+    void addNode(GraphNode *node);
+    void addEdge(GraphNode *source, GraphNode *target);
+    vector<GraphNode *> topologicalSort();
+};
---
+++ src/Graph.cpp
@@ -0,0 +1,89 @@
+#include "../include/graph.h"
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+#ifdef DEBUG_MODE
+#define DEBUG_COMMENT(comment) std::cout << "[DEBUG] " << comment << std::endl;
+#else
+#define DEBUG_COMMENT(comment)
+#endif
+
+GraphNode::GraphNode(string name)
+{
+    this->nodeName = name;
+}
+
+GraphEdge::GraphEdge(GraphNode *source, GraphNode *target)
+{
+    this->source = source;
+    this->target = target;
+}
+
+void Graph::addNode(GraphNode *node)
+{
+    this->nodes.push_back(node);
+}
+
+void Graph::addEdge(GraphNode *source, GraphNode *target)
+{
+    GraphEdge *edge = new GraphEdge(source, target);
+    source->outgoingEdges.push_back(edge);
+}
+
+vector<GraphNode *> Graph::topologicalSort()
+{
+    vector<GraphNode *> sortedOrder;
+    stack<GraphNode *> stack;
+    unordered_map<string, int> visited;
+
+    for (GraphNode *node : nodes)
+    {
+        DEBUG_COMMENT("Inizia Visita " << node->nodeName);
+        if (visited[node->nodeName] == 0 && !visitedNode(node, visited, stack))
+        {
+            DEBUG_COMMENT("Nodo Ciclico: " << node->nodeName << "\n");
+            cerr << "Error: Graph contains a cycle!" << endl;
+            return {}; // Handle cyclic graph
+        }
+    }
+
+    while (!stack.empty())
+    {
+        sortedOrder.push_back(stack.top());
+        stack.pop();
+    }
+
+    return sortedOrder;
+}
+
+bool Graph::visitedNode(GraphNode *node, unordered_map<string, int> &visited, stack<GraphNode *> &stack)
+{
+    DEBUG_COMMENT("Visiting: " << node->nodeName << "\n");
+
+    if (visited[node->nodeName] == 1)
+    {
+        return false; // Detected a back edge, indicating a cycle
+    }
+
+    if (visited[node->nodeName] == 2)
+    {
+        return true; // Already visited
+    }
+
+    visited[node->nodeName] = 1;
+
+    for (GraphEdge *edge : node->outgoingEdges)
+    {
+        if (!visitedNode(edge->target, visited, stack))
+        {
+            return false; // Cyclic graph
+        }
+    }
+
+    visited[node->nodeName] = 2;
+    stack.push(node);
+
+    return true;
+}
